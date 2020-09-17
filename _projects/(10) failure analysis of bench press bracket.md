54530c894d61e6449c7e20403b0593462f3df005
Matthew Gothard <matthew.q.gothard@vanderbilt.edu>
2020-09-17 16:24:23 -0500

    updated og:image and typing words

--- _projects/(10) failure analysis of bench press bracket.md
+++ _projects/(10) failure analysis of bench press bracket.md
@@ -2,10 +2,13 @@
 name: Fatigue Analysis of a Bench Press Bracket 
 tools: [CAD, FEA, Failure Analysis]
 image: "/assets/images/FailureAnalysisOfABenchPressBracket/cad.png"
-description: Preventing pain while getting gains.
+description: Preventing disaster while lifting faster.
 ---
 <!-- Google ModelViewer Script -->
 <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+<!-- <script src=" https://unpkg.com/focus-visible@5.1.0/dist/focus-visible.js" defer></script> -->
 <script nomodule src="https://unpkg.com/@google/model-viewer/dist/model-viewer-legacy.js"></script>
 
-<model-viewer src = "\assets\3d\benchpress\squatrackassembly.glb" alt="A 3D model of a squat rack" auto-rotate camera-controls></model-viewer>
+
+
+<model-viewer style = "margin:0 auto; width:50%; height:400px" src = "\assets\3d\benchpress\squatrackassembly.glb" alt="A 3D model of the squat rack" auto-rotate camera-controls></model-viewer> 
--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["create art with code.", "design robots.", "build 3d printers.", "breadboard circuits.", "sketch out design concepts.", "teach myself new skills.", "take apart electronics."];
+const textArray = ["design robots.", "create art with code.", "build 3d printers.", "breadboard circuits.", "sketch out design concepts.", "teach myself new skills.", "make kinetic art."];
 // const textArray = ['do mechanical design for NASA.', "build 3D printers.",  "play baseball.", "breadboard circuits.", "create art with code.", "go hiking."]
 const typingDelay = 100;
 const erasingDelay = 50;
@@ -11,33 +11,33 @@ let charIndex = 0;
 
 function type() {
   if (charIndex < textArray[textArrayIndex].length) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
     typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
     charIndex++;
     setTimeout(type, typingDelay);
-  } 
+  }
   else {
     cursorSpan.classList.remove("typing");
-  	setTimeout(erase, newTextDelay);
+    setTimeout(erase, newTextDelay);
   }
 }
 
 
 function erase() {
-	if (charIndex > 0) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
     charIndex--;
     setTimeout(erase, erasingDelay);
-  } 
+  }
   else {
     cursorSpan.classList.remove("typing");
     textArrayIndex++;
-    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
     setTimeout(type, typingDelay + 1100);
   }
 }
 
-document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-  if(textArray.length) setTimeout(type, newTextDelay + 250);
+document.addEventListener("DOMContentLoaded", function () { // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
 });
